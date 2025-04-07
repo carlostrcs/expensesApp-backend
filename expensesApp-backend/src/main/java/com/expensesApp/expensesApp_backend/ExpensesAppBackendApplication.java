9619dee0f2e14c9ada79cf37a2e4e1e21dbaa0fa
CARLOS TORRILLAS CASAS <Carlos.Torrillas@alu.uclm.es>
2025-04-07 22:32:20 +0200

    First commit

---
+++ expensesApp-backend/src/main/java/com/expensesApp/expensesApp_backend/ExpensesAppBackendApplication.java
@@ -0,0 +1,14 @@
+package com.expensesApp.expensesApp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesAppBackendApplication.class, args);
+		// comment
+	}
+
+}
---
+++ expensesApp-backend/src/test/java/com/expensesApp/expensesApp_backend/ExpensesAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensesApp.expensesApp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
